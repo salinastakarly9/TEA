d3d5447a979802146742cc7d607fc01a73e16ab2
Takarly Fabiola Salinas Nazar (24262) <takarly.salinas@est.zamorano.edu>
2022-09-08 11:09:25 -0600

    Ejercicio 8.1

---
+++ imprimirnombre.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+nombre = input("Cual es tu nombre?")
+print("Hola"+ nombre)
+edad = input("Cual es su edad")
+calculo = int(edad) * 10
+print(calculo)
---
+++ lab01/lab01.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovación en Tecnología Agrícola
+# Autor: Takarly Fabiola Salinas Nazar 
+# Fecha: 2022.09.01
+# Editador: 2022.09.01
+
+print("Hola mundo!!")
