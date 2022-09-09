6b211f76e88a62904b412a9ae21543b0a5ccaf93
Takarly Fabiola Salinas Nazar (24262) <takarly.salinas@est.zamorano.edu>
2022-09-09 08:20:36 -0600

    Ejercicio 8.2

---
+++ lab02/pagoporhora.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tendencias Agricola (TEA)
+
+horasTrabajadas = input("Ingrese las horas trabajadas: ")
+valorHora = input("Ingrese el valor por hora: ")
+pagoHora = int(horasTrabajadas) * float(valorHora)
+print(pagoHora)
