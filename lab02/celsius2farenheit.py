7ce38b358bf72919f978d53248f01edf8bf15362
Takarly Fabiola Salinas Nazar (24262) <takarly.salinas@est.zamorano.edu>
2022-09-09 08:35:31 -0600

    Ejercicio 8.3

---
+++ lab02/celsius2farenheit.py
@@ -0,0 +1,5 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+
+celsius = input("Ingrese temeperatura en grados Celsius: ")
+farenheit = (float(celsius)) * 9/5 + 32
+print("La temperatura en grados Farenheit es: ", farenheit)
